c91a411e38e540a9e9970c1aa56db7fcfdd29dfe
Frank Kobe <fkobe3@gmail.com>
2022-09-05 19:47:56 -0400

    created server.js file

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const path = require('path');
+const helpers = require('./utils/helpers');
+const exphbs = require('express-handlebars');
+
+
+const hbs = exphbs.create({ helpers });
+
+const session = require('express-session');
+
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'Shhhh, its a secret',
+    rolling: true,
+    cookie: {
+        httpOnly: true,
+        maxAge: 60 * 10000,
+        sameSite: 'strict',
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(session(sess));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
